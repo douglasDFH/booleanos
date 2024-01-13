a477ee9bfb31bd5ef70e784a8fd1e2fdc7fdb5d2
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-12 23:54:14 -0400

    datos_booleanos

---
+++ booleanos_datos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//declaracoin y uso de bool
+	bool b0;
+	b0=true;
+	cout<<"variable con valor true:"<<b0<<endl;
+	b0=false;
+	cout<<"variable con valor false:"<<b0<<endl;
+	// asignacion de valoresnumericoa..
+	//si es cero es FALSO, si es diferentede cero es VERDADERO
+	b0=0;
+	cout<<"VARIABLE CON VALOR 0:"<<b0<<endl;
+	b0=1;
+	cout<<"VARIABLE CON VALOR 1:"<<b0<<endl;
+	b0=-100;
+	cout<<"VARIABLE CON VALOR -100:"<<b0<<endl;
+	return 0;
+}
+
